9a013a4382b3b0f6cba52941ba9e3c8ca59e3e3a
Jen C <jenjch8@gmail.com>
2020-01-15 21:16:13 -0600

    added html routes to notes and index pages

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,19 @@
+
+// dependencies; need to include the path package to get the correct file path for our html
+var path = require("path");
+
+// ROUTING
+module.exports = function(app) {
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+  
+    // this route directs to the notes html
+    app.get("/notes", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+  
+    // If no matching route is found default to home, the index file (home page with start button that leads to notes page)
+    app.get("*", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+  };
