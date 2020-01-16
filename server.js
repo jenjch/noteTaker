4bc0868692a5536f2230b95c1456618b5aa8ed35
Jen C <jenjch8@gmail.com>
2020-01-15 19:40:18 -0600

    adding starting server.js code

---
+++ server.js
@@ -0,0 +1,30 @@
+
+// dependencies
+var express = require("express");
+
+// sets up Express app
+var app = express();
+var PORT = 3000;
+
+// sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+
+// starts the server to begin listening
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+
+
+// for the notes homework if you are getting CORS issues try
+// npm i cors
+// var cors = require("cors")
+// app.use(cors())
+// app.options("*", cors())
+// this will allow any domain to use your API
